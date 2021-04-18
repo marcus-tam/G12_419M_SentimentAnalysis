06b0051b3639cb6e44dbe3134c58dd528f664440
fatean1 <51172100+fatean1@users.noreply.github.com>
2021-04-18 08:53:14 +0800

    Add files via upload

--- process_tweets.py
+++ process_tweets.py
@@ -62,6 +62,7 @@ def bag_of_words(filepath_data,filepath_keywords):
         count+=1
         if indent!=0:
             for i in range(0,indent):
+                str=file_data.readline()
         # if count>=10000:
         #     break
     file_data.close()
@@ -70,8 +71,74 @@ def bag_of_words(filepath_data,filepath_keywords):
     # print(keywords_vectors)
     return keywords_vectors
 
+def convertToID(filepath_data,filepath_keywords):
+    frequent_keywords=[[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+    keywords_vectors=[]
+    file_keywords = open(filepath_keywords,"r")   
+    for i in range (0,26) :
+        str=file_keywords.readline()
+        if(str==""):
+            continue
+        keywords_list=str.split(",")
+        for keyword in keywords_list:
+            frequent_keywords[i].append(keyword)
+    file_keywords.close()
+    start=numpy.zeros(26)
+    start[0]=0
+    for i in range(1,26):
+        start[i]=start[i-1]+len(frequent_keywords[i-1])
+    file_data = open(filepath_data,"r")  
+    str=file_data.readline()
+    length=0
+    lengths=[]
+    while str!="":
+        line_splited=str.split("	")
+        word_str=line_splited[len(line_splited)-2]
+        word_list=word_str.split(" ")
+        lengths.append(len(word_list))
+        if len(word_list)>length:
+            length=len(word_list)
+        str=file_data.readline()
+    file_data.close()
+
+    file_data = open(filepath_data,"r")  
+    str=file_data.readline()
+
+    while str!="":
+        ifdelete=True
+        keywords_vector_temp=[]
+        line_splited=str.split("	")
+        word_str=line_splited[len(line_splited)-2]
+        word_list=word_str.split(" ")     
+        current_index=0 
+        for word in word_list:
+            if len(word)==0:
+                continue
+            index=ord(word[0])-97
+            if index<0 or index>25:
+                continue
+            for i in range(0,len(frequent_keywords[index])):
+                if frequent_keywords[index][i]==word:
+                    ifdelete=False
+                    id=start[index]+i
+                    keywords_vector_temp.append(id)
+                    current_index+=1
+                    break
+        if ifdelete:
+            str=file_data.readline()
+            continue  
+        # for a in range(current_index,length):
+        #       keywords_vector_temp.append(0)
+        keywords_vector_temp.append(line_splited[1][0:10])
+        keywords_vectors.append(keywords_vector_temp)
+        str=file_data.readline()
+    file_data.close()
+    # print(keywords_vectors)
+    # print(set(lengths))
+    return keywords_vectors
+
 if __name__=='__main__':
-    keywords_vectors=bag_of_words("PFE_new.txt","PFE_frequent.txt")
+    keywords_vectors=convertToID("trainMCD_new.txt","MCD_frequent.txt")
     print(len(keywords_vectors))
     
 
