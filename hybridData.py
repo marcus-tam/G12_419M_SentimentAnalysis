9d7ea7afc777756530f096928d3e5126a04c7b10
JackPrice98 <71404252+JackPrice98@users.noreply.github.com>
2021-05-06 15:49:27 -0600

    Script for combinning stocktwits and reddit data

---
+++ hybridData.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def combinedData(twits_path, red_path, use, symbol):
+    twits = (pd.read_csv(twits_path, sep = "\t", header = None)).drop([3], axis = 1)
+    red = pd.read_csv(red_path, sep = "\t", header = None)
+    combo = twits.append(red)
+    path = use + symbol + "Combination.txt"
+    combo.to_csv(path, sep = "\t", header = None, index = False)
+
+if __name__=='__main__':
+    combinedData("testAAPL.txt","testRedditAAPL.txt", "test", "AAPL")
